20edf392c2ceaa891679f33682f051e7c4adb5cf
Simad9 <wijdanbita@gmail.com>
2023-10-02 08:38:33 +0700

    updt::tambah codingan

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+// Bagaian Password
+const hidePass = document.querySelector(".bxs-hide");
+const inputPass = document.querySelector('input[type="password"]');
+
+hidePass.addEventListener("click", () => {
+  hidePass.classList.toggle("bxs-show");
+  const type =
+    inputPass.getAttribute("type") === "password" ? "text" : "password";
+  inputPass.setAttribute("type", type);
+});
+
+// Simulasi
+const submit = document.querySelector(".submit");
+const forgetPass = document.getElementById("forgetPass");
+submit.addEventListener("click", () => {
+  alert("Data sedang terkirim");
+});
+forgetPass.addEventListener("click", () => {
+  alert("Menuju halaman lupa password");
+});
